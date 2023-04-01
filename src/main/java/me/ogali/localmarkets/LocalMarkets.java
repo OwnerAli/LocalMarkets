8f15ec6e2067805ecfbdd2d1cfa47ad520a61061
OwnerAli <alturfiali@gmail.com>
2023-04-01 00:18:16 -0700

    Initial commit

---
+++ src/main/java/me/ogali/localmarkets/LocalMarkets.java
@@ -0,0 +1,17 @@
+package me.ogali.localmarkets;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LocalMarkets extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
